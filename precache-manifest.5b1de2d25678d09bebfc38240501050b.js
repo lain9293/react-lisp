24643366465a0ed8c10c433300acdfa4a0ecff86
Владимир Коротаев <korotaev@axsoft.ru>
2020-09-24 12:51:31 +0300

    Updates

---
+++ precache-manifest.5b1de2d25678d09bebfc38240501050b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58813c430227b0fbfbdb11820cd171b0",
+    "url": "/react-lisp/index.html"
+  },
+  {
+    "revision": "aa8bc7d4e08dee739461",
+    "url": "/react-lisp/static/css/main.eb95ce45.chunk.css"
+  },
+  {
+    "revision": "be09ec3ebc2757527bb0",
+    "url": "/react-lisp/static/js/2.05980a30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lisp/static/js/2.05980a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8bc7d4e08dee739461",
+    "url": "/react-lisp/static/js/main.601762c0.chunk.js"
+  },
+  {
+    "revision": "37af5bfecfc379323f7c",
+    "url": "/react-lisp/static/js/runtime-main.6b2693b6.js"
+  }
+]);
